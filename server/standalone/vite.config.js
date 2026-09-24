cc70de74f1fb879fcd2e94fd9642291c63250d5a
barmand56-rgb <matteomussard56@gmail.com>
2026-09-25 03:28:35 +0400

    Update vite.config.js

--- server/standalone/vite.config.js
+++ server/standalone/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig(({ command, mode }) => {
     cesiumToken: process.env.CESIUM_ION_TOKEN,
     host: process.env.HOST,
     port: process.env.PORT,
+    allowedHosts: true,
     command,
   });
 });
